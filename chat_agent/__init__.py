6d5c51079263ee259d41f0d51d3e1e384de094cd
akswnd98 <akswnd98@gmail.com>
2025-02-12 13:32:19 +0900

    first

--- chat_agent/__init__.py
+++ chat_agent/__init__.py
@@ -1,10 +1,49 @@
+from chat_history import ChatHistory, ConcreteChatHistory, UserChatStep
+from function_call_history import FunctionCallHistory, ConcreteFunctionCallHistory
+
 class AgentAction:
-  pass
+  def get_json (self):
+    pass
+
+class AnswerAction (AgentAction):
+  _answer: str
+
+  def __init__ (self, answer):
+    self._answer = answer
+
+  def get_json (self):
+    return {
+      "answer": self._answer
+    }
+
+class FunctionCallAction (AgentAction):
+  def get_json (self):
+    return {
+      "name": self._function_name,
+      "arguments": self._arguments
+    }
 
 class ChatAgent:
-  def invoke (chat_history: ChatHistory, function_call_history: FunctionCallHistory) -> AgentAction:
+  def invoke (self, chat_history: ChatHistory, function_call_history: FunctionCallHistory) -> AgentAction:
     pass
 
 class ChatAgentExecutor:
-  def invoke (human_input: str):
+  def invoke (self, human_input: str):
     pass
+
+class ConcreteChatAgentExecutor (ChatAgentExecutor):
+  _chat_history: ChatHistory
+  _function_call_history: FunctionCallHistory
+  _chat_agnet: ChatAgent
+
+  def __init__ (self, chat_agent: ChatAgent):
+    self._chat_history = ConcreteChatHistory()
+    self._function_call_history = ConcreteFunctionCallHistory()
+    self._chat_agnet = chat_agent
+
+  def invoke (self, human_input: str):
+    self._function_call_history = ConcreteFunctionCallHistory()
+    self._chat_history.append_history(UserChatStep(human_input))
+    agent_action = None
+    while not isinstance(agent_action, AnswerAction):
+      agent_action = self._chat_agnet.invoke(self._chat_history, self._function_call_history)
--- chat_agent/function_call_history/__init__.py
+++ chat_agent/function_call_history/__init__.py
@@ -1,2 +1,41 @@
+class FunctionCallStep:
+  def get_json (self):
+    pass
+
 class FunctionCallHistory:
-  pass
+  def get_json (self):
+    pass
+
+class ConcreteFunctionCallStep (FunctionCallStep):
+  _thought: str
+  _function_name: str
+  _arguments: any
+  _function_call_result: str
+
+  def __init__ (self, thought: str, function_name: str, arguments: any, function_call_result: str):
+    self._thought = thought
+    self._function_name = function_name
+    self._arguments = arguments
+    self._function_call_result = function_call_result
+
+  def get_json (self):
+    return {
+      "thought": self._thought,
+      "function_call": {
+        "name": self._function_name,
+        "arguments": self._arguments
+      },
+      "function_call_result": self._function_call_result
+    }
+
+class ConcreteFunctionCallHistory (FunctionCallHistory):
+  _function_call_list: list[FunctionCallStep]
+
+  def __init__ (self):
+    self._function_call_list = []
+
+  def get_json (self):
+    return list(map(lambda function_call_step: function_call_step.get_json(), self._function_call_list))
+  
+  def append_history (self, function_call_step: FunctionCallStep):
+    self._chat_list.append(function_call_step)
