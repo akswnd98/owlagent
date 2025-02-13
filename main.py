7192dc0f6d47a6e5f51262769afe9135b401e995
akswnd98 <akswnd98@gmail.com>
2025-02-14 00:38:20 +0900

    first

---
+++ main.py
@@ -0,0 +1,5 @@
+from deepseek_r1_agent import DeepseekR1Agent
+from json_is_all_for_context.agent.agent_executor import AgentExecutor
+
+agent = DeepseekR1Agent("../DeepSeek-R1-Distill-Qwen-14B-Q4_0.gguf")
+AgentExecutor()
